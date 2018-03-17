abd83942920eadc92da965d4baaef5ff9529d10a
madelgj <madelgj@gmail.com>
2018-03-17 13:00:10 +0100

    Configuracion modulo bluetooth
    Con esto al cambiar el codigo en upload ya se sube directamente al arduino

---
+++ configuracionmodulobt.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>   // Incluimos la librería  SoftwareSerial  
+SoftwareSerial BT(10,11);    // Definimos los pines RX y TX del Arduino conectados al Bluetooth
+//Creamos dos pines cualesquiera para que hagan de tx y rx
+ //CONFIGURACION DEL MODULO CON COMANDOS AT
+void setup()
+{
+  BT.begin(9600);       // Inicializamos el puerto serie BT (Para Modo AT 2)
+  Serial.begin(9600);   // Inicializamos  el puerto serie  
+}
+ 
+void loop()
+{
+  if(BT.available())    // Si llega un dato por el puerto BT se envía al monitor serial
+  {
+    Serial.write(BT.read());
+  }
+ 
+  if(Serial.available())  // Si llega un dato por el monitor serial se envía al puerto BT
+  {
+     BT.write(Serial.read());
+  }
+}
+//Pasar comandos AT para configurarlo
